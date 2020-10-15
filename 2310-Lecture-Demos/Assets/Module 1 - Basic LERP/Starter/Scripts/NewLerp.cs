f106c3524da2688b1fc7f38059622f55177c76c5
Matthew North <northmatt1@gmail.com>
2020-10-15 14:32:16 -0400

    Stuff

---
+++ 2310-Lecture-Demos/Assets/Module 1 - Basic LERP/Starter/Scripts/NewLerp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VectorMath {
+    public static Vector3 LERP(Vector3 p0, Vector3 p1, float time) {
+        return p0 * (1 - time) + p1 * time;
+    }
+}
+
+public class NewLerp : MonoBehaviour {
+    public Transform p0;
+    public Transform p1;
+    public float speed = 1;
+    private Transform objTrans;
+    private float time = 0;
+    private bool flippedMovement = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        objTrans = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (flippedMovement && time == 0f || !flippedMovement && time == 1f) {
+            speed *= -1;
+            flippedMovement = !flippedMovement;
+        }
+
+        time = Mathf.Clamp01(time + speed * Time.deltaTime);
+
+        /*    \/ better perf cuz doing less checks and operations
+         if (flippedMovement) {
+            time -= speed * Time.deltaTime;
+
+            if (time <= 0) {
+                time = 0;
+                flippedMovement = false;
+            }
+        } else {
+            time += speed * Time.deltaTime;
+
+            if (time >= 1) {
+                time = 1;
+                flippedMovement = true;
+            }
+        }*/
+
+        objTrans.position = VectorMath.LERP(p0.position, p1.position, time);
+    }
+}
--- 2310-Lecture-Demos/Assets/Module 5 - Bezier Splines/Starter/Scripts/BezierMover.cs
+++ 2310-Lecture-Demos/Assets/Module 5 - Bezier Splines/Starter/Scripts/BezierMover.cs
@@ -9,13 +9,15 @@ public class BezierDemo
     public static Vector3 Bezier3(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
     {
         //TODO: Implement Bezier interpolation.
-        return Vector3.Lerp(p0, p3, t);
+        float s = 1 / t - 1;
+        return t * t * t * (s * (s * (p0 * s + 3 * p1) + 3 * p2) + p3);
+        //return Vector3.Lerp(Bezier2(p0, p1, p2, t), Bezier2(p1, p2, p3, t), t);
     }
 
     public static Vector3 Bezier2(Vector3 p0, Vector3 p1, Vector3 p2, float t)
     {
         //TODO: Implement Bezier.
-        return Vector3.zero;
+        return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
     }
 }
 
@@ -27,29 +29,59 @@ public class BezierMover : MonoBehaviour
     //Just as with Catmull, we won't learn how to control speed directly until next week.
     public float segmentTime;
 
+    private bool goodSegment = false;
     private int currentIndex = 0;
-    private float t;
+    private float t = 0f;
+    private int p0, p1, p2, p3;
 
     void Start()
     {
+        goodSegment = points.Count >= 6 && points.Count % 3 == 0;
+
         if (segmentTime <= 0)
             segmentTime = 1.0f;
 
         //TODO: Initialize our position.
+        if (points.Count > 0)
+            transform.position = points[0].transform.position;
+
+        StartSegment(0);
     }
 
     void Update()
     {
         //It only makes sense to do a Bezier loop if we have at least 6 points.
         //(2 waypoints on path + 2 control points for each of 2 segments).
-        if (points.Count >= 6)
-        {
+        if (goodSegment) {
             //TODO: What's our update look like?
+            t += Time.deltaTime / segmentTime;
+
+            if (t >= 1f)
+                StartSegment(currentIndex + 3);
+
+            transform.position = BezierDemo.Bezier3(points[p0].position,
+                                                    points[p1].position,
+                                                    points[p2].position,
+                                                    points[p3].position, t);
         }
     }
 
-    void StartSegment(int startIndex)
-    {
+    void StartSegment(int startIndex) {
         //TODO: Complete this function.
+        currentIndex = (startIndex >= points.Count) ? 0 : startIndex;
+        p0 = currentIndex;
+        p1 = p0 + 1;
+        if (p1 >= points.Count)
+            p1 = 0;
+
+        p2 = p1 + 1;
+        if (p2 >= points.Count)
+            p2 = 0;
+
+        p3 = p2 + 1;
+        if (p3 >= points.Count)
+            p3 = 0;
+
+        t -= 1f;
     }
 }
